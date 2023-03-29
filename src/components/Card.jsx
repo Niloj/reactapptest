d39ef42a4c675cdbe476e46c9e634c96b673b9f6
Francis Jolin <francis_jolin@intuit.com>
2023-03-29 14:40:25 -0700

    new cards

---
+++ src/components/Card.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './card.scss';
+
+const Card = (props) => {
+  const { img, title, children } = props;
+  
+  return(
+    <div className="card">
+      <div className="image-container">
+        <img src={img} alt="" />
+      </div>
+      <div className='card-body'>
+        <h1>{title}</h1>
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default Card;
